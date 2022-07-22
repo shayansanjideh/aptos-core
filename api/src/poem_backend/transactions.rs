64c8c08e9583f70b36b11dd2e057ac01c985760b
Daniel Porteous <danielporteous1@gmail.com>
2022-07-21 21:11:50 -0700

    [API] Rework response and error handling

--- api/src/poem_backend/transactions.rs
+++ api/src/poem_backend/transactions.rs
@@ -4,25 +4,22 @@
 // Copyright (c) Aptos
 // SPDX-License-Identifier: Apache-2.0
 
-use std::convert::TryFrom;
 use std::sync::Arc;
 
-use super::accept_type::AcceptType;
+use super::accept_type::{parse_accept, AcceptType};
 use super::page::Page;
-use super::{response::AptosResponseResult, ApiTags, AptosResponse};
-use super::{AptosError, AptosErrorCode, AptosErrorResponse};
+use super::AptosErrorCode;
+use super::{
+    ApiTags, AptosErrorResponse, BasicErrorWith404, BasicResponse, BasicResponseStatus,
+    BasicResultWith404, InternalError,
+};
 use crate::context::Context;
 use crate::failpoint::fail_point_poem;
-use anyhow::format_err;
-use aptos_api_types::AsConverter;
-use aptos_api_types::{LedgerInfo, Transaction, TransactionOnChainData};
+use anyhow::Context as AnyhowContext;
+use aptos_api_types::{AsConverter, LedgerInfo, Transaction, TransactionOnChainData};
 use poem::web::Accept;
 use poem_openapi::param::Query;
-use poem_openapi::payload::Json;
 use poem_openapi::OpenApi;
-
-// TODO: Make a helper that builds an AptosResponse from just an anyhow error,
-// that assumes that it's an internal error. We can use .context() add more info.
 
 pub struct TransactionsApi {
     pub context: Arc<Context>,
@@ -44,38 +41,34 @@ impl TransactionsApi {
         accept: Accept,
         start: Query<Option<u64>>,
         limit: Query<Option<u16>>,
-    ) -> AptosResponseResult<Vec<Transaction>> {
-        fail_point_poem("endpoint_get_transactions")?;
-        let accept_type = AcceptType::try_from(&accept)?;
+    ) -> BasicResultWith404<Vec<Transaction>> {
+        fail_point_poem::<BasicErrorWith404>("endppoint_get_transactions")?;
+        let accept_type = parse_accept::<BasicErrorWith404>(&accept)?;
         let page = Page::new(start.0, limit.0);
         self.list(&accept_type, page)
     }
 }
 
 impl TransactionsApi {
-    fn list(&self, accept_type: &AcceptType, page: Page) -> AptosResponseResult<Vec<Transaction>> {
-        let latest_ledger_info = self.context.get_latest_ledger_info_poem()?;
+    fn list(&self, accept_type: &AcceptType, page: Page) -> BasicResultWith404<Vec<Transaction>> {
+        let latest_ledger_info = self
+            .context
+            .get_latest_ledger_info_poem::<BasicErrorWith404>()?;
         let ledger_version = latest_ledger_info.version();
-        let limit = page.limit()?;
+        let limit = page.limit::<BasicErrorWith404>()?;
         let last_page_start = if ledger_version > (limit as u64) {
             ledger_version - (limit as u64)
         } else {
             0
         };
-        let start_version = page.start(last_page_start, ledger_version)?;
+        let start_version = page.start::<BasicErrorWith404>(last_page_start, ledger_version)?;
 
         let data = self
             .context
             .get_transactions(start_version, limit, ledger_version)
-            .map_err(|e| {
-                AptosErrorResponse::InternalServerError(Json(
-                    AptosError::new(
-                        format_err!("Failed to read raw transactions from storage: {}", e)
-                            .to_string(),
-                    )
-                    .error_code(AptosErrorCode::InvalidBcsInStorageError),
-                ))
-            })?;
+            .context("Failed to read raw transactions from storage")
+            .map_err(BasicErrorWith404::internal)
+            .map_err(|e| e.error_code(AptosErrorCode::InvalidBcsInStorageError))?;
 
         self.render_transactions(data, accept_type, &latest_ledger_info)
     }
@@ -85,12 +78,18 @@ impl TransactionsApi {
         data: Vec<TransactionOnChainData>,
         accept_type: &AcceptType,
         latest_ledger_info: &LedgerInfo,
-    ) -> AptosResponseResult<Vec<Transaction>> {
+    ) -> BasicResultWith404<Vec<Transaction>> {
         if data.is_empty() {
-            return AptosResponse::try_from_rust_value(vec![], latest_ledger_info, accept_type);
+            let data: Vec<Transaction> = vec![];
+            return BasicResponse::try_from_rust_value::<BasicErrorWith404>((
+                data,
+                latest_ledger_info,
+                BasicResponseStatus::Ok,
+                accept_type,
+            ));
         }
 
-        let resolver = self.context.move_resolver_poem()?;
+        let resolver = self.context.move_resolver_poem::<BasicErrorWith404>()?;
         let converter = resolver.as_converter();
         let txns: Vec<Transaction> = data
             .into_iter()
@@ -101,16 +100,14 @@ impl TransactionsApi {
                 Ok(txn)
             })
             .collect::<Result<_, anyhow::Error>>()
-            .map_err(|e| {
-                AptosErrorResponse::InternalServerError(Json(
-                    AptosError::new(
-                        format_err!("Failed to convert transaction data from storage: {}", e)
-                            .to_string(),
-                    )
-                    .error_code(AptosErrorCode::InvalidBcsInStorageError),
-                ))
-            })?;
+            .context("Failed to convert transaction data from storage")
+            .map_err(BasicErrorWith404::internal)?;
 
-        AptosResponse::try_from_rust_value(txns, latest_ledger_info, accept_type)
+        BasicResponse::try_from_rust_value((
+            txns,
+            latest_ledger_info,
+            BasicResponseStatus::Ok,
+            accept_type,
+        ))
     }
 }
